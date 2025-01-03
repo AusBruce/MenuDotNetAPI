d42866a9d835af3b591355ed8c7489a534b7c8aa
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-01-04 00:23:27 +1100

    add http get by item id

--- RedMango_API/Controllers/MenuItem.cs
+++ RedMango_API/Controllers/MenuItem.cs
@@ -1,25 +1,52 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RedMango_API.Data;
+using RedMango_API.Models;
+using System.Net;
 
 namespace RedMango_API.Controllers
 {
-    [Route("api/MeunItem")] 
+    [Route("api/MenuItem")]
     [ApiController]
-    public class MenuItem : ControllerBase
+    public class MenuItemController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
-
-        public MenuItem(ApplicationDbContext db)
+        private readonly ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
         {
             _db = db;
+
+            _response = new ApiResponse();
         }
 
-        [HttpGet]   
-
+        [HttpGet]
         public async Task<IActionResult> GetItemsMenu()
         {
-            return Ok( _db.MenuItems);
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemMenu(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+
+            RedMango_API.Models.MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
         }
     }
 }
